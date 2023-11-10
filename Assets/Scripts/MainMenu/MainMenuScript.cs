37d6b21bab7210ca8bb83426e4193b58e5a164fd
Anthony Lethuillier <147057340+CamembearGames@users.noreply.github.com>
2023-11-10 14:55:29 +0100

    menu graphic update

--- Assets/Scripts/MainMenu/MainMenuScript.cs
+++ Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -1,10 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuScript : MonoBehaviour
 {
+
+    private String[] listOfLanguages = new String[] {"English", "Deutsch"};
+    private int currentLanguage = 0;
+    [SerializeField] public TextMeshProUGUI languageField;
+
+
     public void StartGame(){
         LevelLoader.Load(LevelLoader.Scene.IntroScene);
+    }
+
+    public void LeftArrow(){
+        currentLanguage -= 1;
+        if (currentLanguage == -1)
+        {
+            currentLanguage = 1;
+        }
+        languageField.text = listOfLanguages[currentLanguage];
+        
+    }
+
+    public void RightArrow(){
+        currentLanguage += 1;
+        if (currentLanguage == 2)
+        {
+            currentLanguage = 0;
+        }
+        languageField.text = listOfLanguages[currentLanguage];
+
     }
 }
